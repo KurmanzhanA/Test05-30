c0d177fca7cc3418bd80dee7d54a8cdd7ce5848b
Kurmanzhan <anarbaevakurmanzhan@gmail.com>
2023-06-01 10:29:41 +0600

    'test'

--- class1.py
+++ class1.py
@@ -1,3 +1,12 @@
 class Hello:
     def __init__(self, str):
         self.str = str
+class Goodbye(Hello):
+    def __init__(self, str, name):
+        super().__init__(str)
+        self.name = name
+    def say(self):
+        print(f'{self.str}, {self.name}')
+
+a = Goodbye('Hi', 'stranger')
+a.say()
--- task.py
+++ task.py
@@ -1,2 +1,7 @@
 from art import tprint
 tprint("PYTHON")
+from class1 import Goodbye
+
+a = Goodbye('Hallo', 'Mark')
+tprint(a.str)
+tprint(a.name)
